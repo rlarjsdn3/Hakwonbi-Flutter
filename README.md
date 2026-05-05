b4fbce40bbd9ff5715027e701305756c86c9f3ca
rlarjsdn3 <rlarjsdn3@naver.com>
2026-05-05 20:20:35 +0900

    feat: 프로젝트 생성 및 Flutter 린트 규칙 설정, 의존성 주입

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Java-Jungsuk-Basic
-❰자바의 정석 (4판 Java 21)❱ 예제 코드 모음 
+# 학원비
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/hakwonbi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hakwonbi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
