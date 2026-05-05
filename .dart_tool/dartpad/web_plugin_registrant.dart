2965e9fc6994b483b7df9229aee4df5cc1ffdce3
김소월 <rlarjsdn3@naver.com>
2026-05-05 20:43:36 +0900

    feat: 프로젝트 생성 및 Flutter 린트 규칙 설정, 의존성 주입 (#2)

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,22 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:kakao_map_sdk/web/kakao_map_sdk_web.dart';
+import 'package:location_web/location_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  KakaoMapWebPlugin.registerWith(registrar);
+  LocationWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
